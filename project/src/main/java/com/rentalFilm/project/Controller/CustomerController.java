3ae56ca432689d273cf3c637b03793bc3ceec585
Aldo Mancuso <mancusoaldo8 @gmail.com>
2023-05-06 16:01:09 +0200

    Add first GetMapping in CustomerController  and CustomerService

--- project/src/main/java/com/rentalFilm/project/Controller/CustomerController.java
+++ project/src/main/java/com/rentalFilm/project/Controller/CustomerController.java
@@ -1,7 +1,43 @@
 package com.rentalFilm.project.Controller;
 
+import com.rentalFilm.project.Exceptions.CustomerNotFoundException;
+import com.rentalFilm.project.Repositories.CustomerRepository;
+import com.rentalFilm.project.Service.CustomerService;
+import jakarta.persistence.GeneratedValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/customer")
 public class CustomerController {
+
+    @Autowired
+    CustomerService customerService;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    Logger logger = LoggerFactory.getLogger(CustomerController.class);
+
+
+    @GetMapping("/{Id}")
+    public ResponseEntity getCustomerByID(@PathVariable Long Id){
+        try{
+            logger.info("try to find a customer by ID" + Id);
+            return ResponseEntity.status(HttpStatus.OK).body(customerService.GetCustomerById(Id));
+        }catch (CustomerNotFoundException e){
+            logger.warn(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e);
+        }
+    }
+
+
 }
--- project/src/main/java/com/rentalFilm/project/Service/CustomerService.java
+++ project/src/main/java/com/rentalFilm/project/Service/CustomerService.java
@@ -1,7 +1,27 @@
 package com.rentalFilm.project.Service;
 
+import com.rentalFilm.project.Entities.Entity.Customer;
+import com.rentalFilm.project.Exceptions.CustomerNotFoundException;
+import com.rentalFilm.project.Repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CustomerService {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+
+    public Customer GetCustomerById(Long id) throws CustomerNotFoundException {
+        Optional<Customer> customerOptional = customerRepository.findById(id);
+        if(customerOptional.isPresent()) {
+            return customerOptional.get();
+        }
+        return null;
+
+
+    }
 }
