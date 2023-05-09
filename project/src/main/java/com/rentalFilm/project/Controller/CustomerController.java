73214a953c3f7f5722937502ceeb623b79b9bf66
Aldo Mancuso <mancusoaldo8 @gmail.com>
2023-05-09 14:23:29 +0200

    Crud added for customer

--- project/src/main/java/com/rentalFilm/project/Controller/CustomerController.java
+++ project/src/main/java/com/rentalFilm/project/Controller/CustomerController.java
@@ -1,6 +1,7 @@
 package com.rentalFilm.project.Controller;
 
 import com.rentalFilm.project.Entities.DTO.CustomerDTO;
+import com.rentalFilm.project.Entities.Entity.Customer;
 import com.rentalFilm.project.Exceptions.CustomerNotFoundException;
 import com.rentalFilm.project.Repositories.CustomerRepository;
 import com.rentalFilm.project.Service.CustomerService;
@@ -12,6 +13,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/customer")
@@ -35,16 +38,39 @@ public class CustomerController {
     }
 
 
-    @GetMapping("/{Id}")
-    public ResponseEntity getCustomerByID(@PathVariable Long Id){
+    @GetMapping("/{id}")
+    public ResponseEntity getCustomerByID(@PathVariable Long id){
         try{
-            logger.info("try to find a customer by ID" + Id);
-            return ResponseEntity.status(HttpStatus.OK).body(customerService.GetCustomerById(Id));
+            logger.info("try to find a customer by ID" + id);
+            return ResponseEntity.status(HttpStatus.OK).body(customerService.GetCustomerById(id));
         }catch (CustomerNotFoundException e){
             logger.warn(e.getMessage());
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e);
         }
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteCustomer(@PathVariable long id){
+        try{
+            logger.info("try to delete customer by id " + id);
+            return ResponseEntity.status(HttpStatus.OK).body(customerService.deleteCustomer(id));
+        } catch (CustomerNotFoundException e) {
+            logger.warn(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+
+    }
+
+    @GetMapping("/all")
+    public  ResponseEntity allCustomer(){
+        try {
+            logger.info("try to show all customers");
+            return ResponseEntity.status(HttpStatus.OK).body(customerService.all());
+        } catch (CustomerNotFoundException e) {
+            logger.warn(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+
+    }
 
 }
--- project/src/main/java/com/rentalFilm/project/Service/CustomerService.java
+++ project/src/main/java/com/rentalFilm/project/Service/CustomerService.java
@@ -2,11 +2,17 @@ package com.rentalFilm.project.Service;
 
 import com.rentalFilm.project.Entities.DTO.CustomerDTO;
 import com.rentalFilm.project.Entities.Entity.Customer;
+import com.rentalFilm.project.Entities.Entity.Film;
 import com.rentalFilm.project.Exceptions.CustomerNotFoundException;
 import com.rentalFilm.project.Repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,19 +24,36 @@ public class CustomerService {
 
     public Customer GetCustomerById(Long id) throws CustomerNotFoundException {
         Optional<Customer> customerOptional = customerRepository.findById(id);
+        if (customerOptional.isEmpty()) throw new CustomerNotFoundException("customer not found");
         if (customerOptional.isPresent()) {
             return customerOptional.get();
+        }return null;
+    }
+
+
+        public Customer create (CustomerDTO customer){
+            Customer customerDB = new Customer();
+            customerDB.setEmail(customer.getEmail());
+            customerDB.setFirstName(customer.getFirstName());
+            customerDB.setLastName(customer.getLastName());
+            return customerRepository.save(customerDB);
         }
-        return null;
+
+        public ResponseEntity deleteCustomer(long id) throws CustomerNotFoundException {
+        Customer customerDB = GetCustomerById(id);
+        customerRepository.delete(customerDB);
+        return ResponseEntity.status(HttpStatus.OK).body("customer deleted " + id);
+        }
 
 
+    public List<Customer> all() throws CustomerNotFoundException {
+        List <Customer> customers = new ArrayList<>();
+        customers = customerRepository.findAll();
+        customers.stream()
+                .findAny()
+                .get()
+                .toString();
+
+        return customers;
     }
-
-    public Customer create(CustomerDTO customer) {
-         Customer customerDB = new Customer();
-       return customerRepository.save(customerDB);
-    }
-
-
 }
-
